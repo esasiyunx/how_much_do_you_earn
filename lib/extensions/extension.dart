5cb140185e7e38d90b904ce52868caa38a83658c
AYDIN YILDIRIM <yildirimaydinx@gmail.com>
2022-02-01 19:01:00 +0300

    sa

--- lib/extensions/extension.dart
+++ lib/extensions/extension.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-void myShowErrorMessage(String text) {
+void exShowErrorMessage(
+    {required String text,
+    Toast? length = Toast.LENGTH_SHORT,
+    ToastGravity? gravity = ToastGravity.CENTER,
+    int timeInSecForIosWeb = 1,
+    Color? backgroundColor = Colors.red,
+    Color? textColor = Colors.white,
+    double? fontSize = 16.0}) {
   Fluttertoast.showToast(
       msg: text,
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      timeInSecForIosWeb: 1,
-      backgroundColor: Colors.red,
-      textColor: Colors.white,
-      fontSize: 16.0);
+      toastLength: length,
+      gravity: gravity,
+      timeInSecForIosWeb: timeInSecForIosWeb,
+      backgroundColor: backgroundColor,
+      textColor: textColor,
+      fontSize: fontSize);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'extensions/extension.dart';
 
-void main() => runApp(MaterialApp(home: MyHomePage()));
+void main() => runApp(const MaterialApp(home: MyHomePage()));
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -19,31 +20,31 @@ class StudentDialogBoxState extends State<MyHomePage> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("appBar Title"),
+          title: const Text("appBar Title"),
           actions: <Widget>[
             IconButton(
-              icon: Icon(Icons.warning),
+              icon: const Icon(Icons.warning),
               tooltip: "Get an alarm.",
               onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text("alert"),
-                        content: Text("test"),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, 'OK'),
-                            child: const Text('OK'),
-                          ),
-                          TextButton(
-                            onPressed: () => Navigator.pop(context, 'Cancel'),
-                            child: const Text('Cancel'),
-                          ),
-                        ],
-                      );
-                    });
-                myShowErrorMessage("kardeş hata");
+                // showDialog(
+                //     context: context,
+                //     builder: (BuildContext context) {
+                //       return AlertDialog(
+                //         title: const Text("alert"),
+                //         content: const Text("test"),
+                //         actions: <Widget>[
+                //           TextButton(
+                //             onPressed: () => Navigator.pop(context, 'OK'),
+                //             child: const Text('OK'),
+                //           ),
+                //           TextButton(
+                //             onPressed: () => Navigator.pop(context, 'Cancel'),
+                //             child: const Text('Cancel'),
+                //           ),
+                //         ],
+                //       );
+                //     });
+                exShowErrorMessage(text: "kardeş hata verdi");
               },
             )
           ],
@@ -57,7 +58,7 @@ class StudentDialogBoxState extends State<MyHomePage> {
                 });
               },
             ),
-            Text("welcome " + studentName)
+            Text("welcome $studentName")
           ],
         ),
       ),
